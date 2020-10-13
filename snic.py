941ad0587ccd14ee4257bcb4a5a3fa9ebdfd5795
Muskan Chitara <56308233+chitara-01@users.noreply.github.com>
2020-10-13 21:35:13 +0530

    All labels 99.0 (incorrect)

--- snic.py
+++ snic.py
@@ -36,10 +36,10 @@ def neighbours(curr_pos, image_size):
     if y-1 >= 0:
         nbh.append([x, y-1])
 
-    if x+1 < image_size[1]:
+    if x+1 < image_size[0]:
         nbh.append([x+1, y])
 
-    if y+1 < image_size[0]:
+    if y+1 < image_size[1]:
         nbh.append([x, y+1])
 
     return nbh
@@ -56,7 +56,7 @@ def initializeCentroid(image_size, num_of_clusters):
     # compute grid size
     num_sqr = sqrt(num_of_clusters)
     
-    full_step = [image_size_x/num_sqr, image_size_y/num_sqr]
+    full_step = [image_size_y/num_sqr, image_size_x/num_sqr]
     half_step = [full_step[0]/2.0, full_step[1]/2.0]
     
     matrix = [[[
@@ -67,25 +67,35 @@ def initializeCentroid(image_size, num_of_clusters):
     
     return centroids
     
-def calculateDistance(curr_pixel, centroid_pixel, num_of_pixels, num_of_clusters):
+def calculateDistance(pos1, c1, pos2, c2, num_of_pixels, num_of_clusters):
     """distance between two pixels"""
     s = sqrt(num_of_pixels/num_of_clusters)
     m = 10
-    dist_norm = LA.norm(curr_pixel.x - centroid_pixel.x)
-    color_norm = LA.norm(curr_pixel.c - centroid_pixel.c)
+    
+    dist_norm = LA.norm(np.array(pos1) - np.array(pos2))
+    color_norm = LA.norm(np.array(c1) - np.array(c2))
+    
     return sqrt(((dist_norm**2)/s) + ((color_norm**2)/m))
     
 def snic(image, compactness, num_of_clusters):
+    print("Starting snic..")
     rows = im.shape[0]
     cols = im.shape[1]
+    image_size = [rows,cols]
     
     num_of_pixels = rows*cols
+    print("rows: ",rows)
+    print("cols: ",cols)
+    print("pixels: ",num_of_pixels)
     
     """ initialize centroids; C = [(x1,y1),...] ---> centroids = [(pos, avg color, num_pixels), ...]"""
     C = initializeCentroid(image.shape, num_of_clusters)
+    print("number of centroids: ", len(C))
+    print("First centroid is (x,y): ", C[0][0], C[0][1])
+    print("Last centroid is (x,y): ", C[99][0], C[99][1])
     
     """ centroids = [[0,0,0]] initially there are 0 pixels"""
-    centroids = [[pos, im[pos[1]][pos[0]], 0] for pos in C]
+    centroids = [[pos, im[pos[0]][pos[1]], 0] for pos in C]
     
     """ initialize labels as 0"""
     labels = np.zeros((rows,cols))
@@ -103,14 +113,15 @@ def snic(image, compactness, num_of_clusters):
     """find label for each pixel"""
     while len(pq) != 0:
         curr_ele = heapq.heappop(pq)
-        x = curr_ele.x
-        K = curr_ele.k
+        x = curr_ele.val[0]
+        c = curr_ele.val[1]
+        K = curr_ele.val[2]
         
-        if labels[x.X][x.Y]==0:
-            labels[x.X][x.Y] = K
+        if labels[x[0]][x[1]] == 0:
+            labels[x[0]][x[1]] = K
             
             """update centroid"""
-            centroid = centroids[K]
+            centroid = centroids[K-1]
             num = centroid[2]+1
             weight = 1/num
             centroid[0] = [
@@ -119,21 +130,21 @@ def snic(image, compactness, num_of_clusters):
                            ]
 
             centroid[1] = [
-                            (centroid[1][0] * (1 - weight)) + (curr_ele.c[0] * weight),
-                            (centroid[1][1] * (1 - weight)) + (curr_ele.c[1] * weight),
-                            (centroid[1][2] * (1 - weight)) + (curr_ele.c[2] * weight)]
+                            (centroid[1][0] * (1 - weight)) + (c[0] * weight),
+                            (centroid[1][1] * (1 - weight)) + (c[1] * weight),
+                            (centroid[1][2] * (1 - weight)) + (c[2] * weight)]
             centroid[2] = num
 
             
             """retrieve neightbours' coordinates; nbh = [(x1,y1),...]"""
-            nbh = neighbours(curr_ele)
+            nbh = neighbours(x, image_size)
             
             for n in nbh:
                 pos = n
                 color = image[pos[0]][pos[1]]
                 
                 value = [pos, color, k]
-                key = calculateDistance(n,C[K],rows*cols,k)
+                key = calculateDistance(n, image[n[0]][n[1]], C[K-1], image[C[K-1][0]][C[K-1][1]], num_of_pixels, k)
                 
                 e = element(key, value)
                 
@@ -162,6 +173,13 @@ compactness = 0.01
 
 labels, centroids = snic(im,compactness,num_of_segments)
 
+print("Shape of labels: ", labels.shape)
+
+print("First 10 distinct labels are: ")
+for l in range(10):
+    i = l*40 + 20
+    print("label no. ", i, 0 ," is: ", labels[i][0])
+
 fig = plt.figure("Segmented output with %d segments " % len(centroids))
-plt.imshow(mark_boundaries(im, np.array(labels), color=(1, 1, 1)))
+plt.imshow(mark_boundaries(im, labels, color=(1, 1, 1)))
 plt.show()
